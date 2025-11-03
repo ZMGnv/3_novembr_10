b8f51f43e6fbf0d4d7edd8fae57d13788fcad27b
ZMGnv <73342220+ZMGnv@users.noreply.github.com>
2025-11-03 10:37:00 +0200

    Izveidots uzdevums1.py
    Uzdevums izveidots, kur jāizlabo kļūdas!

---
+++ uzdevums1.py
@@ -0,0 +1,19 @@
+import math
+# Ievade
+a = float(input("Ievadi a: "))
+b = float(input("Ievadi b: "))
+c = float(input("Ievadi c: ")
+
+# Diskriminants
+D = b**2 - 4*a*c
+
+# Sakņu aprēķins
+if D > 0:
+    x1 = (-b + math.sqrt(D)) / (2*a)
+    x = (-b - math.sqrt(D)) / (2*a)
+    print(f"D = {D}, divas saknes: x1 = {x1}, x2 = {x2}")
+elif D == 0:
+    x = -b / (2*a)
+    print(fD = {D}, viena sakne: x = {x}")
+else:
+    print(f"D = {D}, nav reālu sakņu.")
